4647312b67539630f3d962d2278d9a8b49c7c99f
s3883908 <s3883908@student.rmit.edu.au>
2024-05-17 03:26:08 +1000

    A2 text

--- assignment 2/script.js
+++ assignment 2/script.js
@@ -1,21 +1,36 @@
-const video = document.querySelector("#custom-video-player");
-const playPauseBtn = document.querySelector("#play-pause-btn");
-const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
-function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+
+// video effect
+window.addEventListener('scroll', function() {
+  var section2 = document.getElementById('section2');
+  var section2Top = section2.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+  var customVideoPlayer = document.getElementById('custom-video-player');
+
+  if (section2Top < windowHeight) {
+    customVideoPlayer.classList.add('hidden');
   } else {
-    video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    customVideoPlayer.classList.remove('hidden');
   }
-}
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
-}
-// Add other functionalities here
+});
+
+//header and nav section color change 
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.getElementById("header");
+  const nav = document.getElementById("nav");
+  const section2 = document.getElementById("section2");
+
+  window.addEventListener("scroll", function() {
+      const section2Top = section2.offsetTop;
+      const scrollPos = window.scrollY + window.innerHeight / 2;
+
+      if (scrollPos >= section2Top) {
+         // Add 'light-text' class when section2 is at the top of the viewport
+          header.classList.add("light-text");
+          nav.classList.add("light-text");
+      } else {
+         // Remove 'light-text' class when section2 is not at the top of the viewport
+          header.classList.remove("light-text");
+          nav.classList.remove("light-text");
+      }
+  });
+});
